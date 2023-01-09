8ae175bd24ddce6d49a72445b8f49e04a79165e0
carlos <carlos.santana.espla26@gmail.com>
2023-01-09 17:26:57 +0000

    third version

--- src/main/java/AemetWeatherSensor.java
+++ src/main/java/AemetWeatherSensor.java
@@ -7,9 +7,10 @@ import org.jsoup.Jsoup;
 import java.io.IOException;
 
 public class AemetWeatherSensor {
-    public AemetWeatherSensor() throws IOException {
+    private String apiKey = "eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJjYXJsb3Muc2FudGFuYS5lc3BsYTI2QGdtYWlsLmNvbSIsImp0aSI6ImY4YmU0ZjhjLWIyMzItNGRhNC04Nzc1LWMzMjgwM2VmNTY2OSIsImlzcyI6IkFFTUVUIiwiaWF0IjoxNjczMjYyNTQwLCJ1c2VySWQiOiJmOGJlNGY4Yy1iMjMyLTRkYTQtODc3NS1jMzI4MDNlZjU2NjkiLCJyb2xlIjoiIn0._SWdpKDj0xQDQYjCrRn5k88M8rsK7JfpLDR_UyWQfac";
+
+    public String DataUrl() throws IOException {
         String url = "https://opendata.aemet.es/opendata/api/observacion/convencional/todas";
-        String apiKey = "eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJjYXJsb3Muc2FudGFuYS5lc3BsYTI2QGdtYWlsLmNvbSIsImp0aSI6ImY4YmU0ZjhjLWIyMzItNGRhNC04Nzc1LWMzMjgwM2VmNTY2OSIsImlzcyI6IkFFTUVUIiwiaWF0IjoxNjczMjYyNTQwLCJ1c2VySWQiOiJmOGJlNGY4Yy1iMjMyLTRkYTQtODc3NS1jMzI4MDNlZjU2NjkiLCJyb2xlIjoiIn0._SWdpKDj0xQDQYjCrRn5k88M8rsK7JfpLDR_UyWQfac";
         String response = Jsoup.connect(url)
                 .followRedirects(false)
                 .timeout(6000)
@@ -18,18 +19,20 @@ public class AemetWeatherSensor {
                 .header("api_key", apiKey)
                 .method(Connection.Method.GET)
                 .maxBodySize(0).execute().body();
-        Gson gson = new Gson();
-        String res = request(url);
-        JsonObject jsonObject = gson.fromJson(res, JsonObject.class);
-        String data = jsonObject.getAsJsonPrimitive("datos").getAsString();
-        String content = request(data);
-        JsonArray jsonElements = gson.fromJson(content, JsonArray.class);
 
-
+        JsonObject jsonObject = new Gson().fromJson(response, JsonObject.class);
+        String dataUrl = jsonObject.get("datos").getAsString();
+        return dataUrl;
     }
-
-    private String request(String url) {
-        return url;
+    public String getContent(String dataUrl) throws IOException {
+        String content = Jsoup.connect(dataUrl)
+                .followRedirects(false)
+                .timeout(6000)
+                .ignoreContentType(true)
+                .header("accept", "application/json")
+                .header("api_key", apiKey)
+                .method(Connection.Method.GET)
+                .maxBodySize(0).execute().body();
+        return content;
     }
-
 }
